9bbee2f5c501247d0a58bcd05353c09cd4b52513
COD-truth <ekanetony123@gmail.com>
2026-05-10 16:17:26 +0200

    Added vite.config.ts.timestamp-1778422645546-a1ca72fa434618.mjs

--- src/components/ui/sonner(1).tsx
+++
@@ -1,6 +0,0 @@
-import { Toaster as Sonner } from "sonner";
-type ToasterProps = React.ComponentProps<typeof Sonner>;
-const Toaster = ({ ...props }: ToasterProps) => (
-  <Sonner theme="dark" className="toaster group" {...props} />
-);
-export { Toaster };
