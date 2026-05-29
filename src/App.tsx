bfd1f9b213f9f832485a8e7f034a66e4e1cbef22
COD-truth <ekanetony123@gmail.com>
2026-05-29 18:00:11 +0200

    Add server sync

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import { useServerSync } from "@/hooks/useServerSync";
 
-const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
-);
+function AppInner() {
+  useServerSync();
+  return (
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  );
+}
+
+const App = () => <AppInner />;
 
 export default App;
